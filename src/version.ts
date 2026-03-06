06f7f42ac02c9f14e1e1215995bcc6f4ae86a44a
Sri Panyam <sri.panyam@gmail.com>
2026-03-06 09:33:56 -0800

    mcp in gitignore

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 // Auto-generated - do not edit manually
-export const VERSION = '0.0.77';
-export const BUILD_DATE = '2026-03-06T09:29:03.343Z';
+export const VERSION = '0.0.90';
+export const BUILD_DATE = '2026-03-06T17:32:12.966Z';
 export const BUILD_INFO = `Excaliframe v${VERSION} (built ${BUILD_DATE})`;
