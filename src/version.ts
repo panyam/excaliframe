2fbe2612ab319bcdc3661f5128398ef3b9018f76
Sri Panyam <sri.panyam@gmail.com>
2026-03-08 17:31:40 -0700

    Increasing slomo time

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 // Auto-generated - do not edit manually
-export const VERSION = '0.0.141';
-export const BUILD_DATE = '2026-03-08T22:56:43.429Z';
+export const VERSION = '0.0.142';
+export const BUILD_DATE = '2026-03-09T00:29:22.189Z';
 export const BUILD_INFO = `Excaliframe v${VERSION} (built ${BUILD_DATE})`;
