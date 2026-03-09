ff98c3dcd66db6396e9df9fae6c74a13e28858d8
Sri Panyam <sri.panyam@gmail.com>
2026-03-08 20:20:40 -0700

    Extra flag to control sharing

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 // Auto-generated - do not edit manually
-export const VERSION = '0.0.168';
-export const BUILD_DATE = '2026-03-09T03:16:02.544Z';
+export const VERSION = '0.0.169';
+export const BUILD_DATE = '2026-03-09T03:18:18.598Z';
 export const BUILD_INFO = `Excaliframe v${VERSION} (built ${BUILD_DATE})`;
