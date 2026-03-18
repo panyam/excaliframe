94e99cdf6c7c1842bf14dfe66fc9ff580bca8b1d
Sri Panyam <sri.panyam@gmail.com>
2026-03-17 23:47:20 -0700

    Version up

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 // Auto-generated - do not edit manually
-export const VERSION = '0.0.177';
-export const BUILD_DATE = '2026-03-09T07:51:57.821Z';
+export const VERSION = '0.0.180';
+export const BUILD_DATE = '2026-03-18T06:42:11.938Z';
 export const BUILD_INFO = `Excaliframe v${VERSION} (built ${BUILD_DATE})`;
